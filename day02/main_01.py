24f4c85b9c3bbb861b114e506b3e444537e26491
Pedro Sousa <pedronsousa@gmail.com>
2021-12-14 11:16:00 +0000

    Day 02 - Part 1

---
+++ day02/main_01.py
@@ -0,0 +1,15 @@
+import helpers
+
+lines = helpers.read_input(2, 1)
+
+commands = tuple((v[0], int(v[1])) for v in (v.split() for v in lines))
+
+position, depth = (0, 0)
+
+for command, units in commands:
+    match(command):
+        case 'forward': position += units
+        case 'down': depth += units
+        case 'up': depth -= units
+
+print(position * depth)
