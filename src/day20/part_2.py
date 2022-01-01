c0064c5d808d02c80e8bf94c52e99fc1759dfc5a
Pedro Sousa <pedronsousa@gmail.com>
2022-01-01 19:53:41 +0000

    Day 20 - Part 2

--- src/day20/part_2.py
+++ src/day20/part_2.py
@@ -1,7 +1,14 @@
 from src.global_helpers import read_input
 
+from src.day20.helpers import enhance_multiple
+
+
 def main():
-  lines = read_input(20, 1, True)
-  return 0
+    lines = read_input(20, 1)
+
+    light_count = enhance_multiple(lines, 50)
+
+    return light_count
+
 
 print(main())
--- tests/day_20_test.py
+++ tests/day_20_test.py
@@ -4,3 +4,7 @@ import src.day20.part_2
 
 def test_part_1():
     assert src.day20.part_1.main() == 5065
+
+
+def test_part_2():
+    assert src.day20.part_2.main() == 14790
