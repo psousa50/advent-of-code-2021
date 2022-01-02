3bb58aed389c7c4a88b6ec2c77382c8b5c1b90f7
Pedro Sousa <pedronsousa@gmail.com>
2022-01-02 16:21:44 +0000

    Day 20 - Refactor

--- src/day20/helpers.py
+++ src/day20/helpers.py
@@ -5,24 +5,10 @@ from src.global_models import Point, Surface
 
 
 def all_neighbours(image: Surface, p: Point):
-    n = []
-    for dx, dy in [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]:
-        point = Point(p.x + dx, p.y + dy)
-        n.append(point)
-    return n
-
-
-def extract_binary(row_coords, image: Surface, out_pixels_are_on: bool):
-    bytes: List[str] = []
-    for c in row_coords:
-        b = "0"
-        if image.is_valid_coord(c):
-            b = "1" if image[c] == "#" else "0"
-        else:
-            b = "1" if out_pixels_are_on else "0"
-        bytes += b
-
-    return "".join(bytes).ljust(3, "0")
+    return [
+        Point(p.x + dx, p.y + dy)
+        for dx, dy in [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+    ]
 
 
 def enhance_image(image: Surface, algorithm: str, out_pixels_are_on: bool):
@@ -32,12 +18,11 @@ def enhance_image(image: Surface, algorithm: str, out_pixels_are_on: bool):
     for y in range(-1, image.maxY() + 1):
         for x in range(-1, image.maxX() + 1):
             p = Point(x, y)
-            neighbours = all_neighbours(image, p)
-            r1 = extract_binary(neighbours[0:3], image, out_pixels_are_on)
-            r2 = extract_binary(neighbours[3:6], image, out_pixels_are_on)
-            r3 = extract_binary(neighbours[6:9], image, out_pixels_are_on)
-            binary_value = r1 + r2 + r3
-            value = int(binary_value, 2)
+            value = 0
+            for c in all_neighbours(image, p):
+                valid = image.is_valid_coord(c)
+                bit = 1 if valid and image[c] == "#" or not valid and out_pixels_are_on else 0
+                value = 2 * value + bit
             enhanced_image[Point(x + 1, y + 1)] = algorithm[value]
 
     return enhanced_image
