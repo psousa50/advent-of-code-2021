9c92bfd13ddf9bf4d8fe3248893c40d05bd2b431
Pedro Sousa <pedronsousa@gmail.com>
2022-01-08 11:59:41 +0000

    Day 21 - Part 2

--- src/day21/part_2.py
+++ src/day21/part_2.py
@@ -1,10 +1,35 @@
+import functools
+import itertools
+from dataclasses import dataclass
+
 from src.global_helpers import read_input
 
 
-def main():
-    lines = read_input(21, 1, True)
+@functools.cache
+def calc_wins(p1_pos: int, p1_score: int, p2_pos: int, p2_score: int):
+    p1_wins = p2_wins = 0
+    dice = (1, 2, 3)
+    rolls = itertools.product(dice, dice, dice)
+    for r1, r2, r3 in rolls:
+        next_p1_pos = (p1_pos + r1 + r2 + r3 - 1) % 10 + 1
+        next_p1_score = p1_score + next_p1_pos
+        if next_p1_score >= 21:
+            p1_wins += 1
+        else:
+            next_p2_wins, next_p1_wins = calc_wins(p2_pos, p2_score, next_p1_pos, next_p1_score)
+            p1_wins += next_p1_wins
+            p2_wins += next_p2_wins
+    return p1_wins, p2_wins
 
-    return 0
+
+def main():
+    lines = read_input(21, 1)
+
+    players = [int(line.split(" ")[4]) for line in lines]
+
+    p1_wins, p2_wins = calc_wins(players[0], 0, players[1], 0)
+
+    return max(p1_wins, p2_wins)
 
 
 print(main())
--- tests/day_21_test.py
+++ tests/day_21_test.py
@@ -4,3 +4,7 @@ import src.day21.part_2
 
 def test_part_1():
     assert src.day21.part_1.main() == 571032
+
+
+def test_part_2():
+    assert src.day21.part_2.main() == 49975322685009
