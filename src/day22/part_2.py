32dacec9af60001ee0a0890715cf6925cc0ba0c9
Pedro Sousa <pedronsousa@gmail.com>
2022-01-08 19:04:29 +0000

    Day 22 - Part 1

---
+++ src/day22/part_2.py
@@ -0,0 +1,9 @@
+from src.global_helpers import read_input
+
+
+def main():
+    lines = read_input(22, 1, True)
+    return 0
+
+
+print(main())
---
+++ tests/day_22_test.py
@@ -0,0 +1,6 @@
+import src.day22.part_1
+import src.day22.part_2
+
+
+def test_part_1():
+    assert src.day22.part_1.main() == 607657
